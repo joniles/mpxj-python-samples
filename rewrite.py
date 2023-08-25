e1dac25225e90dfa3990449189f73329431a5ebb
joniles <jon.iles@bcs.org.uk>
2023-08-25 09:10:06 +0100

    Add rewrite sample

---
+++ rewrite.py
@@ -0,0 +1,23 @@
+# This code illustrates how to read and write
+# a schedule file using MPXJ's readers and writers directly.
+
+import jpype
+import mpxj
+import tempfile
+
+jpype.startJVM()
+
+# Read a sample file
+from net.sf.mpxj.reader import UniversalProjectReader
+project = UniversalProjectReader().read('example.mpp')
+
+# Create a temporary file name
+tempFile = tempfile.NamedTemporaryFile(suffix='.xml').name
+print(tempFile)
+
+# Write as MSPDI to the temporary file
+from net.sf.mpxj.mspdi import MSPDIWriter
+writer = MSPDIWriter()
+writer.write(project, tempFile)
+
+jpype.shutdownJVM()
